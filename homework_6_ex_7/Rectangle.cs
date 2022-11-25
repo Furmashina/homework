902cb98dae42ec404a8912d79a78c51d9d4c160a
Alex Fursov <jaxx.fursov@gmail.com>
2022-11-25 18:30:18 +0300

    Корректировка к домашке от 25.11

--- homework_6_ex_7/Rectangle.cs
+++ homework_6_ex_7/Rectangle.cs
@@ -8,5 +8,33 @@ namespace homework_6_ex_7
 {
     internal class Rectangle
     {
+        //Создать класс Rectangle с полями a, b (стороны), цвет, поверхность. Поля a, b являются обязательными для заполнения, остальные нет.
+        //Создать перегруженные версии конструкторов, с помощью которых можно проинициализировать объект разными способами, но каждый из которых включает обязательные поля.
+        //Создать метод print, который будет выводить значения полей.
+        public int A;
+        public int B;
+        public string Color;
+        public string Surface;
+
+        public Rectangle(int a, int b)
+        {
+            this.A = a;
+            this.B = b;
+        }
+
+        public Rectangle(int a, int b, string color) : this(a, b)
+        {
+            Color = color;
+        }
+
+        public Rectangle(int a, int b, string color, string surface) : this(a, b, color)
+        {
+            Surface = surface;
+        }
+
+        public void Ptintinfo()
+        {
+            Console.WriteLine($"У данного прямоугольника сторона А - {A}, сторона B - {B}, цвет - {Color}, поверхность - {Surface}");
+        }
     }
 }
---
+++ homework_6_ex_8/cat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6_ex_8
+{
+    internal class cat
+    {
+        //Задача 2 Создать класс Cat с полями имя, возраст, любимое блюдо.Поле имя является обязательным, остальные нет.
+        //Создать несколько перегруженных конструкторов, которые будут связаны между собой для инициализации полей.
+        //Создать метод print, который будет выводить на консоль значения всех полей.
+        public string Name;
+        public int Age;
+        public string FaforiteDish;
+
+        public cat(string name)
+        {
+            Name = name;
+        }
+
+        public cat(string name, int age) : this(name)
+        {
+            Age = age;
+        }
+
+        public cat(string name, int age, string faforiteDish) : this(name, age)
+        {
+            FaforiteDish = faforiteDish;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя кота - {Name}, ему {Age} лет, любит покушать {FaforiteDish}");
+        }
+    }
+}
