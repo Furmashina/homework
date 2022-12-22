5291296091cafc3334c24a45e94e28ef89115aa7
Александр Фурсов <jaxx.fursov@gmail.com>
2022-12-22 20:24:32 +0300

    Корректировка к домашке от 22.12

--- homework_8_ex_4/Program.cs
+++ homework_8_ex_4/Program.cs
@@ -66,10 +66,69 @@ namespace homework_8_ex_4
                         Console.WriteLine($"у счета {BankAccounts[numberAcconuts].number} сумма {BankAccounts[numberAcconuts].summ}");
                         break;
                     case 3:
+                        //3.Сделать перевод.Юзер вводит номер счета-отправителя, номер счета-получателя и сумму перевода.
+                        //Сделайте проверку, что баланса отправителя хватает для перевода
+                        bool approveCount = false;
+                        Console.WriteLine("Ввелите индекс с которого перевести деньги");
+                        int indexCount1 = int.Parse(Console.ReadLine());
+                        int indexTrasfer1 = BankAccounts.IndexOf(indexCount1);
+                        Console.WriteLine("Введите индекс, на который надо перевести деньги");
+                        int indexCount2 = int.Parse(Console.ReadLine());
+                        int indexTransfer2 = BankAccounts.IndexOf(indexCount2);
+                        if (indexCount1 != -1 && indexCount2 != -1)
+                        {
+                            approveCount = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Индекс не найден");
+                        }
+                        if (approveCount)
+                        {
+                            for (int i = 0; i < BankAccounts.Count; i++)
+                            {
+                                Console.WriteLine("Введите сумму для перевода");
+                                int summTransfer = int.Parse(Console.ReadLine());
+                                if (BankAccounts[i].summ < summTransfer)
+                                {
+                                    Console.WriteLine("Недостаточно средств");
+                                }
+                                else
+                                {
+                                    BankAccounts[indexCount1].summ -= summTransfer;
+                                    BankAccounts[indexCount2].summ += summTransfer;
+                                }
+                            }
+                        }
                         break;
                     case 4:
+                        //4.Найти счет с самым большим процентом на остаток(найти целый объект)
+                        double indexMax = 0;
+                        int indexPercent = -1;
+                        for (int i = 0; i < BankAccounts.Count; i++)
+                        {
+                            if (BankAccounts[i].percent > indexMax)
+                            {
+                                indexMax = BankAccounts[i].percent;
+                                indexPercent = i;
+                            }
+                        }
+                        Console.WriteLine($"Самый большой процент будет у счёта {BankAccounts[indexPercent].number} и его процент составляет {BankAccounts[indexPercent].percent} ");
                         break;
                     case 5:
+                        //5.Найти счет, владелец которого заработает больше всех процентов, если будет держать деньги на нем целый год.Прибыль за год вычисляется по формуле сумма на счету*
+                        //процент на остаток / 100
+                        double maxDohod = 0;
+                        int indexMaxDohod = -1;
+                        for (int i = 0; i < BankAccounts.Count; i++)
+                        {
+                            if (BankAccounts[i].summ * BankAccounts[i].percent / 100 > indexMaxDohod)
+                            {
+                                maxDohod = BankAccounts[i].summ * BankAccounts[i].percent / 100;
+                                indexMaxDohod = i;
+                            }
+                        }
+                        Console.WriteLine($"максимальная прибыль за год будет у счёта - {BankAccounts[indexMaxDohod].number} с процентной ставкой - {BankAccounts[indexMaxDohod].percent}\n И будет составлять {maxDohod}");
                         break;
                 }
             }
